b353b9f851897dfd8e642e0d1c669962da252933
umeshkuwar15 <umeshkuwar54321@gmail.com>
2025-12-18 22:20:11 +0530

    [ADDED] this is basic pointer

---
+++ Basicpointer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int *p;      
+
+    p = &a;      
+
+    printf("Value of a = %d\n", a);
+    printf("Address of a = %p\n", &a);
+    printf("Value stored in p = %p\n", p);
+    printf("Value pointed by p = %d\n", *p);
+
+    return 0;
+}
+
