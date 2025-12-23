b68e5514c9c708aab187ce941d81a993dfbac9e4
umeshkuwar15 <umeshkuwar54321@gmail.com>
2025-12-23 21:34:33 +0530

    [ADDED] Its simple Union code

---
+++ Union.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+union Data {
+    int i;
+    float f;
+    char c;
+};
+
+int main() {
+    union Data d;
+
+    d.i = 10;
+    printf("Integer: %d\n", d.i);
+
+    d.f = 3.14;
+    printf("Float: %.2f\n", d.f);
+
+    d.c = 'A';
+    printf("Character: %c\n", d.c);
+
+    return 0;
+}
+
